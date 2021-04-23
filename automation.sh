6b215467b3d1b54fa9bb305462e1e87bfb303b46
Ubuntu <ubuntu@ip-172-31-17-161.ec2.internal>
2021-04-23 16:33:04 +0000

    shell script to move logs

---
+++ automation.sh
@@ -0,0 +1,47 @@
+#! /bin/bash
+
+echo "Updating packages.. "
+sudo apt update -y &> /dev/null
+
+dpkg -s apache2 &> /dev/null
+
+if [ $? -eq 0 ]
+then
+    echo "Apache2 is installed. Skipping Apache2 installation."
+else
+    echo "Apache2 is not installed. Installing"
+    sudo apt install apache2 -y &> /dev/null
+fi
+
+sudo service apache2 status &> /dev/null
+
+if [ $? -eq 0 ]
+then
+    echo "Apache2 service is running"
+else
+    echo "Apache2 service is not running.. starting Apache2 service."
+    sudo service apache2 start &> /dev/null
+
+    if [ $? -eq 0 ]
+    then
+        echo "Apache2 service started successfully."
+    else
+        echo "Failed to start Apache2 service."
+    fi
+fi
+
+echo "Creating archive of Apache2 log files"
+timestamp=$(date '+%d%m%Y-%H%M%S')
+filename="venkatesh-httpd-logs-$timestamp.tar.gz"
+echo $filename
+
+cur_dir=$(pwd)
+cd /var/log/apache2
+sudo tar -czf $filename *.log
+
+sudo mv *.gz /tmp/
+cd $cur_dir
+
+echo "Uploading $filename to S3 bucket.."
+aws s3 cp /tmp/$filename s3://upgrad-venkatesh//$filename
+
